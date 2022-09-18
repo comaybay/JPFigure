24d9fb0ee5f8bd92d30dd819f09607205b609f40
Thái Chí Bảo <comaybayy@gmail.com>
2022-09-18 19:04:03 +0700

    Add project files.

---
+++ JPFigureContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace JPFigure
+{
+	public class JPFigureContext : DbContext
+	{
+		private readonly IConfiguration config;
+
+		protected JPFigureContext(IConfiguration config)
+		{
+			Console.Write(config);
+			this.config = config;
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+			=> optionsBuilder.UseNpgsql(config.GetConnectionString("JPFigurines"));
+	}
+}
